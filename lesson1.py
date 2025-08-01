62be660c26736ea4012e4930e6812da8b5174fae
johnalain <98930620+johnalain@users.noreply.github.com>
2025-08-01 12:52:27 +0300

    Add files via upload
    changes2

---
+++ lesson1.py
@@ -0,0 +1,29 @@
+#https://youtu.be/rM-jDeSgOQw?t=5657
+# nesting functions :
+    # creating shorter and cleaner code with nesting functioms
+    #putting functionsinside of each other
+    # how do we nest:
+            # height =  int(input("enter height")) #
+
+# understand the first two things:
+        # know which functions can be nested
+        # how to form them correctly
+        #  ex: 
+            #height = input ("enter your height")
+            # height = int(height)
+                #same as above
+            # height = int(input("enter your height"))
+# word = input("enter a random word")
+# color = input("enter random color")
+# length_1 = len(word)
+# length_2 = len(color)
+# total =length_1 + length_2
+# print("total letters" , total)
+# ===== using nested functios ======
+# word = input("enter a random word")
+# color = input("enter random color")
+length_1 = len(input("enter a random word: "))
+length_2 = len(input("enter random color: "))
+total =length_1 + length_2
+print("total letters" , total)
+#https://youtu.be/rM-jDeSgOQw?t=6088
